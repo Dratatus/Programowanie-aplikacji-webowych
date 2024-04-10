f0a5481668874cfc5831a3e32a76b853c4656b6d
Daniel <daniel.partatus@onet.pl>
2024-04-10 09:39:20 +0200

    users mock

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,9 @@
+export type UserRole = 'Admin' | 'Developer' | 'DevOps';
+
+
 export interface User {
     id: number;
     firstName: string;
     lastName: string;
+    role: UserRole;
   }
--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -3,6 +3,11 @@ import { User } from '../models/user';
 class AuthService {
   private static instance: AuthService;
   private currentUser: User | null = null;
+  private users: User[] = [
+    { id: 1, firstName: 'Tytus', lastName: 'Bomba', role: 'Admin' },
+    { id: 2, firstName: 'Romek', lastName: 'Atomek', role: 'Developer' },
+    { id: 3, firstName: 'Alojzy', lastName: 'Mortadela', role: 'DevOps' }
+  ];
 
   private constructor() {}
 
@@ -13,14 +18,21 @@ class AuthService {
     return AuthService.instance;
   }
 
-  public loginUser(): User {
-    const mockUser: User = { id: 1, firstName: 'Tytus', lastName: 'Bomba' };
-    this.currentUser = mockUser;
-    return mockUser;
+  public loginUser(id: number = 1): User {
+    const user = this.users.find(user => user.id === id);
+    if (user) {
+      this.currentUser = user;
+      return user;
+    }
+    throw new Error("User not found");
   }
 
   public getCurrentUser(): User | null {
     return this.currentUser;
+  }
+
+  public getUsers(): User[] {
+    return this.users;
   }
 }
 
